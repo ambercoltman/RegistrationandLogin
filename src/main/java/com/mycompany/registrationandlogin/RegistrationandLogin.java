2a1cc5f41ec98507194b77515868b29603602e6f
ambercoltman <ST10438993@vcconnect.edu.za>
2024-04-08 20:00:34 +0200

    Changes

--- src/main/java/com/mycompany/registrationandlogin/RegistrationandLogin.java
+++ src/main/java/com/mycompany/registrationandlogin/RegistrationandLogin.java
@@ -12,35 +12,30 @@ import java.util.Scanner;
 public class RegistrationandLogin {
 
     public static void main(String[] args) {
-      LoginUser user = new LoginUser();
-        
-        Scanner in= new Scanner(System.in);
-        
-        
-        System.out.println("Enter your username:");
-        
-        System.out.println("Username is not correctly formatted, please ensure"
-                    + "that your username contains an undercore and is no more than"
-                  + "5 character in length");
-                
-      
-            }
-       }
-    
-
-
-
-
-
-
-
- //System.out.println("Username succesfully captured");
-    
-        
-    
+      LoginUser user = new LoginUser();       
+      Scanner in = new Scanner(System.in);
+   //   username = new Username  ();
+   
      
-    
-    
-
-
-
+      System.out.println("Enter your first name:");
+      in.nextLine();
+      
+      System.out.println("Enter your last name:");
+      in.nextLine();
+     
+      System.out.println("Enter your username:");
+      in.nextLine();
+      user.checkUserName("true");
+      System.out.println("Username successfully captured.");
+      in.nextLine();
+      user.checkUserName("false");
+      System.out.println("Username is not correctly formatted, please ensure that your username contains an undercore and is no more than 5 character in length.");
+      in.nextLine();        
+      System.out.println("Enter your password:");
+      in.nextLine();
+     
+      
+    }
+}
+           
+     
