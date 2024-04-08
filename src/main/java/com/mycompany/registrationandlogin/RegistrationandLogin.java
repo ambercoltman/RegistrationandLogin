7e64e4150447ea9608bb3666b57982a95263d2c0
ambercoltman <ST10438993@vcconnect.edu.za>
2024-04-08 11:24:48 +0200

    changes

---
+++ src/main/java/com/mycompany/registrationandlogin/RegistrationandLogin.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrationandlogin;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class RegistrationandLogin {
+
+    public static void main(String[] args) {
+      LoginUser user = new LoginUser();
+        
+        Scanner in= new Scanner(System.in);
+        
+        
+        System.out.println("Enter your username:");
+        
+        System.out.println("Username is not correctly formatted, please ensure"
+                    + "that your username contains an undercore and is no more than"
+                  + "5 character in length");
+                
+      
+            }
+       }
+    
+
+
+
+
+
+
+
+ //System.out.println("Username succesfully captured");
+    
+        
+    
+     
+    
+    
+
+
+
