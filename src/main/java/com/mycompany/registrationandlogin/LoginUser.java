72e694472de4176cb66e2186aa3da29e6ac43191
ambercoltman <ST10438993@vcconnect.edu.za>
2024-04-13 17:43:33 +0200

    changes

--- src/main/java/com/mycompany/registrationandlogin/LoginUser.java
+++ src/main/java/com/mycompany/registrationandlogin/LoginUser.java
@@ -19,8 +19,9 @@ package com.mycompany.registrationandlogin;
      public String password2;
      
      public boolean checkUserName(String username) {
-         
-       if(username.length() <= 5 && username.contains("_")){
+       String length = "<=5";  
+       if(username.contains(length) && username.contains("_")){
+           
        System.out.println("Username successfully captured.");
        }else{
        System.out.println("Username is not correctly formatted, please ensure that your username contains an undercore and is no more than 5 character in length.");
@@ -36,31 +37,32 @@ package com.mycompany.registrationandlogin;
        String specialCharacter = "(?=.*[@#$%^&*!.)";
        
        if(password.contains(length) && password.contains(oneDigit) && password.contains(lowerCase) && password.contains(upperCase) && password.contains(specialCharacter)){
-         System.out.println("Password successfully captured.");
-         }else{
          System.out.println("Password is not correctly formatted, please ensure that the  password contains at least 8 characters, a capital letter, a number and a special character.");
+         }else{
+         System.out.println("Password successfully captured.");
         }
-         return false;
-        
+         return false;  
      }     
      
      public String registerUser(){
      
-         if(!checkUserName(username)){
-         System.out.println("The username is incorrectly formatted.");
-     }
-         if(!checkPasswordComplexity(password)) {
-         System.out.println("The password does not meet the complexity requirements.");
+       if(!checkUserName(username)){
+          System.out.println("The username is incorrectly formatted.");
+         }
+       if(!checkPasswordComplexity(password)) {
+          System.out.println("The password does not meet the complexity requirements.");
          
-        }
-         return "The two above conditions have been met and the user has been registered successfully.";
+         }else{ 
+          System.out.println("The two above conditions have been met and the user has been registered successfully.");
+       }
+         return null;
      
-     }
+      }
      
      public boolean loginUser1(){
        
       if(username == username2 && password == password2){
-          System.out.println("Welcome" + firstName + lastName + "It is great to see you again.");
+          System.out.println("Welcome ,it is great to see you again.");
       }else{
           System.out.println("Username or password incorrect, pease try again.");
       }
